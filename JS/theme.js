4eda6c0c28fcf3388db8341de0df25d15e141fb3
KwaAlice <alicekwa2006@gmail.com>
2026-07-20 11:44:02 +0800

    Create theme.js

---
+++ JS/theme.js
@@ -0,0 +1,39 @@
+const themeToggle = document.getElementById("themeToggle");
+
+window.onload = function(){
+
+    const savedTheme = localStorage.getItem("theme");
+
+    if(savedTheme === "dark"){
+
+        document.body.classList.add("dark-mode");
+        themeToggle.textContent = "☀️";
+
+    }
+    else{
+
+        themeToggle.textContent = "🌙";
+
+    }
+
+};
+
+themeToggle.addEventListener("click",function(){
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        localStorage.setItem("theme","dark");
+        themeToggle.textContent = "☀️";
+
+    }
+
+    else{
+
+        localStorage.setItem("theme","light");
+        themeToggle.textContent = "🌙";
+
+    }
+
+});
